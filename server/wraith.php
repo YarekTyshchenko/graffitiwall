5fb550d6db88d0ab144054acf29349bc0ec076a4
Yarek T <yarekt@gmail.com>
2012-06-24 05:21:18 +0100

    Fixed a bug with wraith

--- server/wraith.php
+++ server/wraith.php
@@ -9,7 +9,7 @@ class Wraith
     protected $_by = array();
 
     protected $_culled = 0;
-    protected $_notCulled = 0;
+    protected $_count = 0;
 
     public function cull()
     {
@@ -22,8 +22,8 @@ class Wraith
         
         $m = memory_get_usage();
         while ($line = $result->fetch_assoc()) {
-            if ($this->_notCulled % 100 == 0) {
-                echo ($this->_notCulled + $this->_culled) . ' ('.$this->_culled.') / ' . $total;
+            if ($this->_count % 100 == 0) {
+                echo ($this->_count) . ' ('.$this->_culled.') / ' . $total;
                 echo ' Buffer '.count($this->_bx).':'.count($this->_by);
                 echo ' Mem: '. number_format(memory_get_usage() - $m).' bytes'.PHP_EOL;
 
@@ -38,13 +38,12 @@ class Wraith
             if ($covered) {
                 $db->delete($line['id']);
                 $this->_culled++;
-            } else {
-                $this->_notCulled++;
             }
+            $this->_count++;
         }
         $result->close();
         $end = microtime(true) - $start;
-        echo 'Culled '. $this->_culled . ' / '.$this->_notCulled.' lines in '. number_format($end, 4) . ' s'.PHP_EOL;
+        echo 'Culled '. $this->_culled . ' / '.$this->_count.' lines in '. number_format($end, 4) . ' s'.PHP_EOL;
     }
 
     protected function _isCovered($x1, $y1, $x2, $y2, $w)
@@ -64,7 +63,7 @@ class Wraith
                 // check for circle intersection
                 $l = sqrt(pow($x, 2) + pow($y, 2));
                 if (floor($l) <= $w) {
-                    if ($this->_check($x1 + $x, $y1 + $y)) {
+                    if (! $this->_check($x1 + $x, $y1 + $y)) {
                         $return = false;
                     }
                 }
@@ -76,7 +75,7 @@ class Wraith
                 for ($y = 0; $y <= (max($y1, $y2) + $w) - (min($y1, $y2) - $w); $y++) {
                     list($d, $outside) = $this->_p($x1, $y1, $x2, $y2, $x1+$x, $y1+$y);
                     if (floor($d) <= $w && !$outside) {
-                        if ($this->_check($x1 + $x, $y1 + $y)) {
+                        if (! $this->_check($x1 + $x, $y1 + $y)) {
                             $return = false;
                         }
                     }
@@ -88,13 +87,12 @@ class Wraith
     }
 
     protected function _check($x, $y) {
-        if (! isset($this->_bx[$x]) && ! array_key_exists($x, $this->_bx)) {
-            $this->_bx[$x] = null;
-            return false;
-        }
-        if (! isset($this->_by[$y]) && ! array_key_exists($y, $this->_by)) {
-            $this->_by[$y] = null;
-            return false;
+        if (! array_key_exists($x, $this->_bx)) {
+            if (! array_key_exists($y, $this->_by)) {
+                $this->_bx[$x] = null;
+                $this->_by[$y] = null;
+                return false;
+            }
         }
         return true;
     }
