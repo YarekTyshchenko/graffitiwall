d7920a10f4568367a0b76f0e0369201509044343
Yarek T <yarek@gmail.com>
2013-06-24 22:32:52 +0100

    Fix broken count because of namespaces

--- server/db.js
+++ server/db.js
@@ -4,8 +4,9 @@ var connection = mysql.createConnection({
     database: 'graffitiwall_websocket'
 });
 
-var _getCountFrom = function(table, callback) {
-    connection.query('SELECT count(*) as count FROM ' + table, function(err, result) {
+var _getCountFrom = function(table, namespace, callback) {
+    namespace = namespace || '';
+    connection.query('SELECT count(*) as count FROM ' + table + ' WHERE namespace = ?', [namespace] function(err, result) {
         callback(result[0].count);
     });
 };
@@ -16,7 +17,7 @@ exports.insert = function(data) {
 
 exports.replay = function(callback, namespace) {
     namespace = namespace || '';
-    _getCountFrom('points', function(total){
+    _getCountFrom('points', namespace, function(total){
         var query = connection.query(
             'SELECT x1, y1, x2, y2, width, color FROM points WHERE namespace = ? ORDER BY id ASC',
             [namespace]
@@ -36,27 +37,5 @@ exports.replay = function(callback, namespace) {
     });
 };
 
-exports.timelapse = function(callback){
-    _getCountFrom('timelapse', function(total){
-        total = 10000;
-        var page = 0;
-        for (var i = Math.ceil(total/1000) - 1; i >= 0; i--) {
-            var query = connection.query(
-                'SELECT x1, y1, x2, y2, width, color FROM timelapse ORDER BY id ASC LIMIT ' + page + ',1000'
-            );
-            page += 1000;
-                console.log(page);
-
-            var list = [];
-            query.on('result', function(row) {
-                list.push(row);
-            }).on('end', function(){
-                var end = false;
-                if (page/1000 >= total) {
-                    end = true;
-                }
-                callback(list, page/1000, total, end);
-            });
-        }
-    });
+exports.timelapse = function(callback, namespace){
 };
