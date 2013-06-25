ceebc92e99748d0064e6691b505b8fd7a64b4a6b
Yarek T <yarekt@gmail.com>
2013-06-25 03:06:13 +0100

    Tweek the mysql pool numbers

--- server/db.js
+++ server/db.js
@@ -1,20 +1,23 @@
 var mysql = require('mysql');
 var pool  = mysql.createPool({
+    connectionLimit: 2,
+    queueLimit: 10,
     user: 'graffitiwall',
     database: 'graffitiwall_websocket'
 });
 
 var _getCountFrom = function(table, namespace, callback) {
     pool.getConnection(function(err, connection) {
+        if (err) { console.log(err); return;}
         connection.query('SELECT count(*) as count FROM ' + table + ' WHERE namespace = ?', [namespace], function(err, result) {
-            connection.end();
-            callback(result[0].count);
+            callback(connection, result[0].count);
         });
     });
 };
 
 exports.insert = function(data) {
     pool.getConnection(function(err, connection) {
+        if (err) {console.log(err); return;}
         connection.query('INSERT INTO points SET ?', data, function() {
             connection.end();
         });
@@ -22,24 +25,22 @@ exports.insert = function(data) {
 };
 
 exports.replay = function(callback, namespace) {
-    _getCountFrom('points', namespace, function(total){
-        pool.getConnection(function(err, connection) {
-            var list = [];
-            var index = 0;
-            connection.query(
-                'SELECT x1, y1, x2, y2, width, color FROM points WHERE namespace = ? ORDER BY id ASC LIMIT 500000',
-                [namespace]
-            ).on('result', function(row) {
-                list.push(row);
-                if (list.length >= 1000) {
-                    index += list.length;
-                    callback(list, index, total, false);
-                    list = [];
-                }
-            }).on('end', function() {
-                callback(list, index += list.length, total, true);
-                connection.end();
-            });
+    _getCountFrom('points', namespace, function(connection, total){
+        var list = [];
+        var index = 0;
+        connection.query(
+            'SELECT x1, y1, x2, y2, width, color FROM points WHERE namespace = ? ORDER BY id ASC LIMIT 500000',
+            [namespace]
+        ).on('result', function(row) {
+            list.push(row);
+            if (list.length >= 1000) {
+                index += list.length;
+                callback(list, index, total, false);
+                list = [];
+            }
+        }).on('end', function() {
+            callback(list, index += list.length, total, true);
+            connection.end();
         });
     });
 };
